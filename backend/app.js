e0b77c59aabe0dd5d801d0cadc620dce6d1dfa7c
JorgeBinaryFactory <j.lopez@binary-factory.de>
2025-06-11 16:53:33 +0200

    Sex_on_the_beach

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,14 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+
+// Servir archivos estáticos desde frontend
+app.use(express.static(path.join(__dirname, '../front')));
+
+// Ruta raíz: enviar el HTML principal
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../front/html', 'principal.html'));
+});
 
 // Middlewares
 app.use(express.json());
@@ -8,16 +17,11 @@ app.use(express.json());
 const userRoutes = require('./routes/userRoutes');
 const chatRoutes = require('./routes/chatRoutes');
 const configRoutes = require('./routes/configRoutes'); 
-const conversationRoutes = require('./routes/chatRoutes');
+const conversationRoutes = require('./routes/conversationRoutes'); 
 
 app.use('/', userRoutes);
 app.use('/api/chat', chatRoutes);
 app.use('/configuraciones', configRoutes);
 app.use('/conversaciones', conversationRoutes);
-
-/*
-app.get('/', (req, res) => {
-  res.send('Bienvenido al servidor');
-});*/
 
 module.exports = app;
