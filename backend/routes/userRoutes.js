c26cd203931937a16e49fb853b31f6f9265597d1
JoseDavidNavarroLopez <josetemil2005@gmail.com>
2025-06-13 18:12:06 +0200

    Update userRoutes.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,8 +5,10 @@ const userController = require('../controllers/userController');
 // Ruta para obtener todos los usuarios
 router.get('/', userController.getAllUsers);
 
-
 // Ruta para crear un usuario
 router.post('/register', userController.createUser);
+
+// Ruta para iniciar sesión
+router.post('/login', userController.login);
 
 module.exports = router;
