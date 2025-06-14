dd7d0a66bbfa9839b0e2406c2f6ac3f086347994
JoseDavidNavarroLopez <josetemil2005@gmail.com>
2025-06-14 11:49:17 +0200

    Update chatController.js

--- backend/controllers/chatController.js
+++ backend/controllers/chatController.js
@@ -185,7 +185,7 @@ module.exports = {
 
 
 async function obtenerHistorialChats(req, res) {
-  const { email } = req.query;
+  const { email } = req.body;
   if (!email) {
     return res.status(400).json({ error: "Email requerido" });
   }
