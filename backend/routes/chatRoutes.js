9282ef91f907fd1f3c5ae8bab5dc9c4134d948ab
JoseDavidNavarroLopez <josetemil2005@gmail.com>
2025-06-14 18:01:07 +0200

    Update chatRoutes.js

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const {
   procesarMensaje,
   obtenerHistorialChats, 
+  obtenerChatPorId,
 } = require("../controllers/chatController");
 
 router.post("/", procesarMensaje);
