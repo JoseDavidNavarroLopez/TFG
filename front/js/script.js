b117b80c03644c625618a98d3bed54641a9182fc
JoseDavidNavarroLopez <josetemil2005@gmail.com>
2025-06-16 17:12:19 +0200

    Update script.js

--- front/js/script.js
+++ front/js/script.js
@@ -493,7 +493,7 @@ function sendMessageBD() {
 
 loadChatHistory(); 
 
-const chatMensaje = document.getElementById('chat-container');
+const chatMensaje = document.getElementById('chat');
 const id_usuario = Number(sessionStorage.getItem('userId'));
 
 const observer = new MutationObserver((mutationsList) => {
