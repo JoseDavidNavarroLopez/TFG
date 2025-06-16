828a1e2b7c2a05dba1710d234ea2373f4dc51349
JoseDavidNavarroLopez <josetemil2005@gmail.com>
2025-06-16 16:51:19 +0200

    Update mensajeRoutes.js

--- backend/routes/mensajeRoutes.js
+++ backend/routes/mensajeRoutes.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const { guardarMensaje } = require("../controllers/guardarMensajeYConversacion");
 
-router.post("/", guardarMensaje); 
+const { crearConversacion, guardarMensaje } = require("../controllers/guardarMensajeYConversacion");
+
+// Ruta para crear nueva conversación
+router.post("/conversacion", crearConversacion);
+
+// Ruta para guardar mensaje en conversación existente
+router.post("/mensaje", guardarMensaje);
 
 module.exports = router;
 
