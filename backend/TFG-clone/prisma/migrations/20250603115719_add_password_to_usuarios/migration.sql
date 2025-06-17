00a32214253ecd9dac12e3c34a02e5cee437d841
JoseDavidNavarroLopez <josetemil2005@gmail.com>
2025-06-17 20:17:46 +0200

    Delete backend/TFG-clone directory

--- backend/TFG-clone/prisma/migrations/20250603115719_add_password_to_usuarios/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `password` on the `usuarios` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `usuarios` DROP COLUMN `password`,
-    ADD COLUMN `contraseña` VARCHAR(255) NULL;
--- backend/TFG-clone/routes/conversationRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const conversationController = require('../controllers/conversationController');
-
-
-router.get('/:userId', conversationController.getConversacionesPorUsuario);
-
-module.exports = router;
-
