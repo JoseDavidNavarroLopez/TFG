cb071e5f4c070edbcd3d08fe4ab2a9c70b3d21cb
JoseDavidNavarroLopez <josetemil2005@gmail.com>
2025-06-17 15:22:04 +0200

    Update chatRoutes.js

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -5,14 +5,13 @@ const {
   obtenerHistorialChats, 
   obtenerChatPorId,
   guardarNuevaConversacion,
+  chatConIA,
 } = require("../controllers/chatController");
 
 router.post("/", procesarMensaje);
 router.post("/historial", obtenerHistorialChats);
 router.get("/:id", obtenerChatPorId);
 router.post("/conver", guardarNuevaConversacion); 
-router.get("/test", (req, res) => {
-  res.send("Ruta /api/chat/test funcionando");
-});
+router.post("/chat", chatConIA);
 
 module.exports = router;
